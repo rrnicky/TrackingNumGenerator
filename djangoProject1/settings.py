90bbcc5daebca5bc6085b127fcf034daa28f3faa
Rashmi <rrcniky@gmail.com>
2024-09-07 21:07:16 +0530

    change db settings

--- djangoProject1/settings.py
+++ djangoProject1/settings.py
@@ -89,7 +89,7 @@ WSGI_APPLICATION = 'djangoProject1.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'tng-db',
         'USER': 'postgres',
         'PASSWORD': 'gestro123',
